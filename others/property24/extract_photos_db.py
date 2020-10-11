9ddbf7c8c9604ee15077f79958bda8f3bc9b48bc
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-10-11 03:37:32 +0000

    stuff

--- others/property24/extract_photos_db.py
+++ others/property24/extract_photos_db.py
@@ -4,6 +4,8 @@ import glob, os
 import sql_connect
 from os import listdir
 from os.path import isfile, join
+import urllib.request
+import time
 
 PHOTOS_PATH = "C://Repos//property-photos"
 
@@ -19,13 +21,56 @@ def write_blob_to_harddisk(blob, filename):
     with open("C://Repos//temp//" + filename, 'wb') as file:
         file.write(blob)
 
-files = [f for f in listdir(PHOTOS_PATH) if isfile(join(PHOTOS_PATH, f))]
-for filename in files:
-    file_blob = convert_to_binary_data(filename)
-    sql_connect.update_photo_blob(file_blob, filename)
+def write_to_blob():
+    files = [f for f in listdir(PHOTOS_PATH) if isfile(join(PHOTOS_PATH, f))]
+
+    for index, filename in enumerate(files):
+        print(f'photo #{index + 1} of {len(files)}')
+
+    try:
+        file_blob = convert_to_binary_data(filename)
+        data = sql_connect.find_photo_record(filename)
+        listing_number = data[1]
+        original_url = data[2]
+        sql_connect.insert_photo_data(listing_number, original_url, filename, file_blob)
+    except Exception as e:
+        print (e)
+
+def get_from_url():
+    results = sql_connect.get_all_photos()
+    photos_count = len(results)
+    for index, data in enumerate(results[:2]):
+        try:
+            listing_number = data[1]
+            original_url = data[2]
+            filename = data[3]
+            path = f"{PHOTOS_PATH}//{filename}"
+            urllib.request.urlretrieve(original_url, path)
+            time.sleep(3)
+        except Exception as e:
+            error_message = str(e)
+            if hasattr(e, 'message'):
+                error_message = e.message
+
+            sql_connect.insert_error_logs(property_url, error_message)
+            print(error_message)
+
+
+try:
+    get_from_url()
+finally:
+    print('done')
+
+# filename = '113885934-13.png'
+# file_blob = convert_to_binary_data(filename)
+# data = sql_connect.find_photo_record(filename)
+# listing_number = data[1]
+# original_url = data[2]
+# sql_connect.insert_photo_data(listing_number, original_url, filename, file_blob)
 
 # # check if blob generated is valid
-# data = sql_connect.find_photo_record('110460317-1.png')
+# data = sql_connect.find_photo_file_record('110460317-1.png')
 # filename = data[3]
 # blob = data[4]
 # write_blob_to_harddisk(blob, filename)
+
