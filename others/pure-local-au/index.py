fc1e087b9d448e671d7e3adcb004068f5d8109a0
Jennifer Fadriquela <jlfadriquela02@gmail.com>
2023-08-14 21:02:11 +1000

    add extract company details

--- others/pure-local-au/index.py
+++ others/pure-local-au/index.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import random
-
+import os
 
 CHROME_DRIVER_PATH = "C://Repos//chromedriver_win32//chromedriver.exe"
 chromeOptions = Options()
@@ -22,18 +22,25 @@ chromeOptions.add_argument('--kiosk')
 WAITING_TIME = 10
 
 class CompanyDetails:  
-    def __init__(self, company, business_type, website, email, location): 
+    def __init__(self, company, business_type, website, email, linkedin, location): 
         self.company = company
         self.business_type = business_type
         self.website = website
         self.email = email
+        self.linkedin = linkedin
         self.location = location
 
 class BusinessTypeDetails:
     def __init__(self, id, type): 
         self.id = id
         self.type = type
-    
+
+def deleteFile(filename):
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
 
 def append_to_csv(data, file_name, is_header):
     # Add contents of list as last row in the csv file
@@ -71,7 +78,7 @@ def load_all_search_results():
         last_height = new_height
 
 
-def get_company_details(company_link):
+def get_company_details(company_link, business_type):
     driver.get(company_link)
 
     WebDriverWait(driver, WAITING_TIME).until(
@@ -79,17 +86,24 @@ def get_company_details(company_link):
     )
 
     business_name_el = driver.find_element(By.XPATH, '//ul[@id="business-contact-details"]/li[contains(text(), "Business Name")]/following-sibling::li[1]')
-    print(business_name_el.text)
+    company = business_name_el.text
 
     business_site_el = driver.find_element(By.XPATH, '//ul[@id="business-contact-details"]/li[contains(text(), "Official Website")]/following-sibling::li[1]')
-    print(business_site_el.text)
+    website = business_site_el.text
 
     business_location_el = driver.find_element(By.XPATH, '//ul[@id="business-contact-details"]/li[contains(text(), "Business Location")]/following-sibling::li[1]')
-    print(business_location_el.text)
+    location = business_location_el.text
+
+    linkedin = ''
+    linkedin_selector = '//ul[@id="business-contact-details"]/li[contains(text(), "Social Links")]/following-sibling::li[1]/a[contains(@href, "linkedin")]'
+    if len(driver.find_elements(By.XPATH, linkedin_selector)) > 0:
+        linkedin_el = driver.find_element(By.XPATH, linkedin_selector)
+        linkedin = linkedin_el.get_attribute('href')
+
+    return CompanyDetails(company, business_type, website, '', linkedin, location)
 
 
-def get_companies_by_type(type_id):
-    print(type_id)
+def get_companies_by_type(type_id, type_name):
     TYPE_URL = f'https://www.purelocal.com.au/search_results?sid={type_id}'
 
     driver.get(TYPE_URL)
@@ -101,12 +115,19 @@ def get_companies_by_type(type_id):
     load_all_search_results()
 
     links = []
-    for link in driver.find_elements(By.XPATH, "//div[@id='LazyResults']//div[@class='hp5new']/a"):
+    for link in driver.find_elements(By.XPATH, "//div[@id='LazyResults']//div[contains(@class, 'hp5')]/a"):
         href = link.get_attribute('href')
         links.append(href)
 
+    OUTPUT_PATH = f'output/{type_name}.csv'
+    deleteFile(OUTPUT_PATH)
+
+    print(len(links))
+
     for link in links:
-        get_company_details(link)
+        company_details = get_company_details(link, type_name)
+        print(company_details)
+        append_to_csv(company_details, OUTPUT_PATH, False)
 
 
 
@@ -124,6 +145,8 @@ def get_business_types():
         EC.presence_of_element_located((By.CSS_SELECTOR, 'select#sid'))
     )
 
+    deleteFile(OUTPUT_PATH)
+
     for option in driver.find_elements(By.XPATH, '//select[@id="sid"]/option'):
         id = option.get_attribute('value')
         type = option.text
@@ -131,12 +154,25 @@ def get_business_types():
         details = BusinessTypeDetails(id, type)
         append_to_csv(details, OUTPUT_PATH, False)
 
+def get_data_by_business_type():
+    CSV_PATH = 'output/business_types.csv'
+    csv_data = pd.read_csv(CSV_PATH, header=None, names=['id','type','status'])
+
+    for i, row in csv_data.iterrows():
+        typeId = row['id']
+        typeName = row['type']
+        csv_data['status'] = 'done'
+
+    csv_data.to_csv(CSV_PATH, index=False)
+
 try:
     start_time = time.time()
     driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, options=chromeOptions)
     # get_business_types()
 
-    get_companies_by_type(285)
+    get_companies_by_type(140, 'Air Conditioning')
+
+    # get_data_by_business_type()
 
     elapsed_time = time.time() - start_time
     print(f'TIME ELAPSED: {elapsed_time}')
